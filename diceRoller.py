6ca65d3ce3c548d471c0698bf65e967f3308e106
clush7113 <clush7113@sohamcollege.org.uk>
2014-05-15 13:12:41 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,5 @@
-#Coin flip program
-#Describe the purpose of this program here.
-
-inport random;time
+#dice rolling program
+import random, time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -10,25 +8,18 @@ s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
-def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+def rollDice():
+    print("rolling.....")
+    roll = random.randint(1,6)
+    return roll
 
 
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
-        print(s2)
-    else roll = 3
-        print(s3)
-    else roll = 4
-        print(s4)
-    else roll = 5
-        print(s5)
-    else roll = 6
-        print(s6)
+def show_dice(roll):
+    rollDie={1:s1,2:s2,3:s3,4:s4,5:s5,6:s6}
+    print(rollDie[roll])
 
-roll
+
+roll=rollDice()
 time.sleep(1)
 show_dice(roll)
+
